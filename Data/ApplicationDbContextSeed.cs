95e0b1534b7640af58f38b3c87a000fb12aba248
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-30 12:17:02 -0500

    Corregir ortografía en Siembra de datos

--- Data/ApplicationDbContextSeed.cs
+++ Data/ApplicationDbContextSeed.cs
@@ -20,15 +20,15 @@ namespace EjemploMVCCursosOnline.Data
 
                 var listaCurso = new List<Curso>()
                 {
-                    new Curso { Titulo = "Titulo del curso de prueba 1", Descripcion = "Descripcion del curso de prueba 1", FechaPublicacion = DateTime.Parse("2020/08/29 12:10:01"), Precio = new Precio { PrecioNormal = 100000, PrecioPromocion = 40000 } },
-                    new Curso { Titulo = "Titulo del curso de prueba 2", Descripcion = "Descripcion del curso de prueba 2", FechaPublicacion = DateTime.Parse("2020/08/29 12:10:01"), Precio = new Precio { PrecioNormal = 200000, PrecioPromocion = 40000 } },
-                    new Curso { Titulo = "Titulo del curso de prueba 3", Descripcion = "Descripcion del curso de prueba 3", FechaPublicacion = DateTime.Parse("2020/08/29 12:13:01"), Precio = new Precio { PrecioNormal = 300000, PrecioPromocion = 40000 } },
-                    new Curso { Titulo = "Titulo del curso de prueba 4", Descripcion = "Descripcion del curso de prueba 4", FechaPublicacion = DateTime.Parse("2020/08/29 12:14:01"), Precio = new Precio { PrecioNormal = 400000, PrecioPromocion = 40000 } },
-                    new Curso { Titulo = "Titulo del curso de prueba 5", Descripcion = "Descripcion del curso de prueba 5", FechaPublicacion = DateTime.Parse("2020/08/29 12:15:01"), Precio = new Precio { PrecioNormal = 500000, PrecioPromocion = 40000 } },
-                    new Curso { Titulo = "Titulo del curso de prueba 6", Descripcion = "Descripcion del curso de prueba 6", FechaPublicacion = DateTime.Parse("2020/08/29 12:16:01"), Precio = new Precio { PrecioNormal = 600000, PrecioPromocion = 40000 } },
-                    new Curso { Titulo = "Titulo del curso de prueba 7", Descripcion = "Descripcion del curso de prueba 7", FechaPublicacion = DateTime.Parse("2020/08/29 12:17:01"), Precio = new Precio { PrecioNormal = 700000, PrecioPromocion = 40000 } },
-                    new Curso { Titulo = "Titulo del curso de prueba 8", Descripcion = "Descripcion del curso de prueba 8", FechaPublicacion = DateTime.Parse("2020/08/29 12:18:01"), Precio = new Precio { PrecioNormal = 800000, PrecioPromocion = 40000 } },
-                    new Curso { Titulo = "Titulo del curso de prueba 9", Descripcion = "Descripcion del curso de prueba 9", FechaPublicacion = DateTime.Parse("2020/08/29 12:19:01"), Precio = new Precio { PrecioNormal = 900000, PrecioPromocion = 40000 } }
+                    new Curso { Titulo = "Título del curso de prueba 1", Descripcion = "Descripción del curso de prueba 1", FechaPublicacion = DateTime.Parse("2020/08/29 12:10:01"), Precio = new Precio { PrecioNormal = 100000, PrecioPromocion = 40000 } },
+                    new Curso { Titulo = "Título del curso de prueba 2", Descripcion = "Descripción del curso de prueba 2", FechaPublicacion = DateTime.Parse("2020/08/29 12:10:01"), Precio = new Precio { PrecioNormal = 200000, PrecioPromocion = 40000 } },
+                    new Curso { Titulo = "Título del curso de prueba 3", Descripcion = "Descripción del curso de prueba 3", FechaPublicacion = DateTime.Parse("2020/08/29 12:13:01"), Precio = new Precio { PrecioNormal = 300000, PrecioPromocion = 40000 } },
+                    new Curso { Titulo = "Título del curso de prueba 4", Descripcion = "Descripción del curso de prueba 4", FechaPublicacion = DateTime.Parse("2020/08/29 12:14:01"), Precio = new Precio { PrecioNormal = 400000, PrecioPromocion = 40000 } },
+                    new Curso { Titulo = "Título del curso de prueba 5", Descripcion = "Descripción del curso de prueba 5", FechaPublicacion = DateTime.Parse("2020/08/29 12:15:01"), Precio = new Precio { PrecioNormal = 500000, PrecioPromocion = 40000 } },
+                    new Curso { Titulo = "Título del curso de prueba 6", Descripcion = "Descripción del curso de prueba 6", FechaPublicacion = DateTime.Parse("2020/08/29 12:16:01"), Precio = new Precio { PrecioNormal = 600000, PrecioPromocion = 40000 } },
+                    new Curso { Titulo = "Título del curso de prueba 7", Descripcion = "Descripción del curso de prueba 7", FechaPublicacion = DateTime.Parse("2020/08/29 12:17:01"), Precio = new Precio { PrecioNormal = 700000, PrecioPromocion = 40000 } },
+                    new Curso { Titulo = "Título del curso de prueba 8", Descripcion = "Descripción del curso de prueba 8", FechaPublicacion = DateTime.Parse("2020/08/29 12:18:01"), Precio = new Precio { PrecioNormal = 800000, PrecioPromocion = 40000 } },
+                    new Curso { Titulo = "Título del curso de prueba 9", Descripcion = "Descripción del curso de prueba 9", FechaPublicacion = DateTime.Parse("2020/08/29 12:19:01"), Precio = new Precio { PrecioNormal = 900000, PrecioPromocion = 40000 } }
                 };
 
                 context.Curso.AddRange(listaCurso);
