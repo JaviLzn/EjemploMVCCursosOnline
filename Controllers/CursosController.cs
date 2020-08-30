45b00db54d3a19cbb2d70b9fc2d2ab23b92008a0
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-29 19:45:09 -0500

    Quitar usings no utilizados

--- Controllers/CursosController.cs
+++ Controllers/CursosController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using EjemploMVCCursosOnline.Data;
+using EjemploMVCCursosOnline.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using EjemploMVCCursosOnline.Data;
-using EjemploMVCCursosOnline.Models;
 
 namespace EjemploMVCCursosOnline.Controllers
 {
