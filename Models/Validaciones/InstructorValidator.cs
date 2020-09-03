bd5951c879f154d82ac44b54cd4ce30e3d083f04
Javier Lozano <ing.javierlozano@hotmail.com>
2020-09-03 09:25:31 -0500

    Implementar en el layout la librería datatables

---
+++ Models/Validaciones/InstructorValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace EjemploMVCCursosOnline.Models.Validaciones
+{
+    public class InstructorValidator : AbstractValidator<Instructor>
+    {
+        public InstructorValidator()
+        {
+            RuleFor(x => x.NombreCompleto).NotEmpty(); //.WithMessage("Debe especificar el título del curso.");
+        }
+    }
+}
